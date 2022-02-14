d77166de9f01180cda95facc13fa3c0dd0502637
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-02-14 08:35:19 +0530

    remove magic numbers

--- src/player/GamePlay.ts
+++ src/player/GamePlay.ts
@@ -3,6 +3,11 @@ import { Game } from '../state/game';
 import { Rules } from '../state/rules';
 
 export class GamePlay {
+  private static readonly STRIKE_BONUS = 10;
+  private static readonly SPARE_BONUS = 10;
+  private static readonly BONUS_ROLLS = 2;
+  private static readonly PTS_PER_PIN = 1;
+
   private roll() {
     return Math.round(Math.random() * 10) + 1;
   }
@@ -12,7 +17,8 @@ export class GamePlay {
     const turnHistory = [];
     let turnPoint = 0;
 
-    const chances = Rules.maxChances + (isFinal && Game.hasBonus[player] ? 2 : 0);
+    const chances =
+      Rules.maxChances + (isFinal && Game.hasBonus[player] ? GamePlay.BONUS_ROLLS : 0);
 
     for (let i = 0; i < chances; i++) {
       prompt()('Press any key to roll');
@@ -23,13 +29,13 @@ export class GamePlay {
       turnHistory.push(rollRes);
       if (rollRes === 10) {
         console.log(i === 0 ? 'STRIKE' : 'SPARE');
-        turnPoint += i === 0 ? 10 : 5;
+        turnPoint += i === 0 ? GamePlay.STRIKE_BONUS : GamePlay.SPARE_BONUS;
         Game.hasBonus[player] = true;
         break;
       }
     }
 
-    turnPoint += turnHistory.reduce((sum, pt) => sum + pt, 0);
+    turnPoint += turnHistory.reduce((sum, knocks) => sum + knocks * GamePlay.PTS_PER_PIN, 0);
 
     console.log(`This turn: ${turnHistory.join(', ')}`);
     console.log(`Player's points: ${turnPoint}`);
